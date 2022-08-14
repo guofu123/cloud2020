adfb06d9df51960b9c7a82a0f4aa449c425651ca
guguofu <3165177264@qq.com>
2022-08-14 11:16:40 +0800

    cloud-eureka-server7001 ：Eureka服务端创建

---
+++ cloud-eureka-server7001/src/main/java/com/atguigu/springcloud/EurekaMain7001.java
@@ -0,0 +1,16 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Eureka 服务端启动类
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaMain7001 {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaMain7001.class, args);
+    }
+}
